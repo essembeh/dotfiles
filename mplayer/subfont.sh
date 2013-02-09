64071bbf88dadea0f54d4e60bf8191fcf3e8fe5c
Sébastien M-B <essembeh@gmail.com>
2013-02-09 15:22:23 +0100

    Initial import

---
+++ mplayer/subfont.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+echo "ln -fs /usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf ~/.mplayer/subfont.ttf"
+ln -fs /usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf ~/.mplayer/subfont.ttf
