6bfa8f0f34ab567c2734d17145b560b39c70075f
Sébastien M-B <seb@essembeh.org>
2015-03-02 21:03:50 +0100

    Update mplayer gnome integration

--- mplayer/subfont.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-echo "ln -fs /usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf ~/.mplayer/subfont.ttf"
-ln -fs /usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf ~/.mplayer/subfont.ttf
