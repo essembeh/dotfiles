c1423125694371fe0f52ad7120b8cc37fe44e96e
Sébastien M-B <seb@essembeh.org>
2015-07-18 01:55:51 +0200

    Setup script

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+ROOT=`dirname "$0"`
+[[ ! "$ROOT" = /* ]] && ROOT="$PWD/$ROOT"
+
+function createLinks {
+  while test -n "$1"; do
+    local SOURCE="$ROOT/$1"
+    local DESTINATION="$HOME/.`basename "$SOURCE"`"
+
+    if test -L "$DESTINATION"; then
+      echo "$DESTINATION is already linked"
+    elif test -e "$DESTINATION"; then
+      echo "$DESTINATION already exists"
+    else
+      ln -vs "$SOURCE" "$DESTINATION"
+    fi
+    shift
+  done
+}
+
+createLinks bash/bashrc bash/alias bash/profile bash/custompath
+createLinks zsh/zshrc zsh/zshenv
+createLinks git/gitconfig
+createLinks tmux/tmux.conf
+createLinks vim
+createLinks lftp
