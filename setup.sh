4b711e615529d6883d48c4f105891cdb23c41317
Sébastien MB <seb@essembeh.org>
2017-06-14 11:11:54 +0200

    Add mpv

--- setup.sh
+++ setup.sh
@@ -1,20 +1,32 @@
 #!/bin/sh
 set -e
 
-createLinks () {
-  while test -n "$1"; do
-    local SOURCE="`pwd`/$1"
-    local DESTINATION="$HOME/.`basename "$SOURCE"`"
+__ln () {
+	if test -L "$2"; then
+		echo "$2 is already linked"
+	elif test -e "$2"; then
+		echo "$2 already exists"
+	else
+		ln -vs "$1" "$2"
+	fi
+}
 
-    if test -L "$DESTINATION"; then
-      echo "$DESTINATION is already linked"
-    elif test -e "$DESTINATION"; then
-      echo "$DESTINATION already exists"
-    else
-      ln -vs "$SOURCE" "$DESTINATION"
-    fi
-    shift
-  done
+__createConfigLinks () {
+	while test -n "$1"; do
+		local SOURCE="`pwd`/$1"
+		local DESTINATION="$HOME/.config/`basename "$SOURCE"`"
+		__ln "$SOURCE" "$DESTINATION"
+		shift
+	done
+}
+
+__createHomeLinks () {
+	while test -n "$1"; do
+		local SOURCE="`pwd`/$1"
+		local DESTINATION="$HOME/.`basename "$SOURCE"`"
+		__ln "$SOURCE" "$DESTINATION"
+		shift
+	done
 }
 
 cd `dirname "$0"`
@@ -22,11 +34,11 @@ cd `dirname "$0"`
 git submodule sync --recursive
 git submodule update --init --recursive
 
-createLinks submodules/oh-my-zsh
-createLinks bash/bashrc bash/alias bash/profile bash/custompath
-createLinks zsh/zshrc zsh/zshenv
-createLinks git/gitconfig
-createLinks tmux/tmux.conf
-createLinks vim
-createLinks lftp
-createLinks mplayer
+__createHomeLinks submodules/oh-my-zsh
+__createHomeLinks bash/bashrc bash/alias bash/profile bash/custompath
+__createHomeLinks zsh/zshrc zsh/zshenv
+__createHomeLinks git/gitconfig
+__createHomeLinks tmux/tmux.conf
+__createHomeLinks vim
+__createHomeLinks lftp
+__createConfigLinks mpv
