3afa0e9b50b94caabef26cf445e859238aa0ad7c
Sébastien MB <seb@essembeh.org>
2021-03-04 21:08:35 +0100

    Update mysql & add gnomeext

--- mysql/newdb.sh
+++ mysql/newdb.sh
@@ -3,18 +3,19 @@
 echo "Usage $0 <USER> <PASSWORD>"
 echo ""
 
-NAME=${1:-jdoe}
-USER="$NAME@localhost"
+USER=${1:-jdoe}
 PASSWORD="$2"
 
 if test -z "$PASSWORD"; then
-	PASSWORD=`head -c64 /dev/random | base64 | head -c16`
+	PASSWORD=$(head -c64 /dev/random | base64 | head -c16)
 fi
 
 echo "mysql -u root -p"
-echo "create user $USER identified by '$PASSWORD';"
-echo "create database $NAME;"
-echo "grant all privileges on $NAME.* to $USER;"
-echo "flush privileges;"
-echo "exit;"
+echo "CREATE USER '$USER'@'%' IDENTIFIED BY '$PASSWORD';"
+echo "CREATE USER '$USER'@'localhost' IDENTIFIED BY '$PASSWORD';"
+echo "CREATE DATABASE $USER;"
+echo "GRANT ALL PRIVILEGES ON $USER.* TO '$USER'@'%';"
+echo "GRANT ALL PRIVILEGES ON $USER.* TO '$USER'@'localhost';"
+echo "FLUSH PRIVILEGES;"
+echo "EXIT;"
 
