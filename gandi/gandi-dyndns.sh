0ce23a2447000638800e6fc1865761391bed0e20
Sébastien MB <seb@essembeh.org>
2022-06-09 20:36:31 +0200

    Update gandi script

--- gandi/gandi-dyndns.sh
+++ gandi/gandi-dyndns.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-set -eu
+set -eux
 
-if [ $# -ne 3 ]; then
+if [ $# -lt 2 -o $# -gt 3 ]; then
     echo "Simple tool to update Gandi.net DNS record using your current IP address
 
 Usage: 
-  $0 <API_KEY> <DOMAIN> <SUBDOMAIN>
+  $0 <API_KEY> <DOMAIN> [SUBDOMAIN]
 
 Example: 
   $0 dQw4w9WgXcQ example.org dynamic
@@ -15,15 +15,17 @@ Example:
 fi
 API_TOKEN="$1"
 DOMAIN="$2"
-SUBDOMAN="$3"
-API_URL="https://api.gandi.net/v5/livedns/domains/$DOMAIN/records/$SUBDOMAN"
+API_URL="https://api.gandi.net/v5/livedns/domains/$DOMAIN/records"
+if [ $# -eq 3 ]; then
+	API_URL="$API_URL/$3"
+fi
 
 echo "Current config:"
 curl -s -X GET "$API_URL" -H "Authorization: Apikey $API_TOKEN"
 echo ""
 MY_ADDRESS=$(curl -s https://ifconfig.io)
 echo "$MY_ADDRESS" | grep -qE "^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$"
-echo "Update $SUBDOMAN.$DOMAIN with address $MY_ADDRESS using api $API_URL"
+echo "Update with address $MY_ADDRESS using api $API_URL"
 curl -s -X PUT "$API_URL" -H "Authorization: Apikey $API_TOKEN" -H "Content-Type: application/json" --data-binary @- << EOF
 {"items":[{"rrset_type":"A","rrset_ttl":3600,"rrset_values":["$MY_ADDRESS"]}]}
 EOF
