b47f3f12e9d4c5f521d91a5a66c07c7798957693
Sébastien MB <seb@essembeh.org>
2022-09-26 22:52:23 +0200

    Update vscodium install script

--- vscodium/install-latest.sh
+++ vscodium/install-latest.sh
@@ -3,32 +3,28 @@ set -eu
 
 EXT=".tar.gz"
 
-ROOT_DIR="${1:-${PWD}}"
+ROOT_DIR="$(realpath "${1:-${PWD}}")"
 
 LATEST_VERSION=$(curl -s https://api.github.com/repos/VSCodium/vscodium/releases/latest | jq -r '.tag_name')
 test -n "$LATEST_VERSION"
 LATEST_URL="https://github.com/VSCodium/vscodium/releases/download/${LATEST_VERSION}/VSCodium-linux-x64-${LATEST_VERSION}${EXT}"
 LATEST_ARTIFACT="${ROOT_DIR}/$(basename "${LATEST_URL}")"
 
-if [ ! -f "${LATEST_ARTIFACT}" ]; then
-    echo "Download version ${LATEST_VERSION} from ${LATEST_URL}"
-    wget -q "${LATEST_URL}" -O "${LATEST_ARTIFACT}"
-else
-    echo "Version ${LATEST_VERSION} is already downloaded: ${LATEST_ARTIFACT}"
-fi
+echo "🧲 Download ${LATEST_URL}"
+wget --continue --progress=dot:mega --tries=0 -q "${LATEST_URL}" -O "${LATEST_ARTIFACT}"
 
 INSTALL_DIR="${LATEST_ARTIFACT%$EXT}"
 if [ ! -d "${INSTALL_DIR}" ]; then
-    echo "Extract ${LATEST_ARTIFACT} to ${INSTALL_DIR}"
+    echo "📂 Extract ${LATEST_ARTIFACT}"
     mkdir "${INSTALL_DIR}"
     tar -C "${INSTALL_DIR}" -xzf "${LATEST_ARTIFACT}"
 else
-    echo "Version ${LATEST_VERSION} is already installed in ${INSTALL_DIR}"
+    echo "💡 Version ${LATEST_VERSION} is already installed in ${INSTALL_DIR}"
 fi
 
 DESKTOP_FILE="${HOME}/.local/share/applications/VSCodium.desktop"
 if [ -d "$(dirname "${DESKTOP_FILE}")" ]; then
-    echo "Generate desktop file ${DESKTOP_FILE}"
+    echo "🔍 Generate desktop file ${DESKTOP_FILE}"
     echo "[Desktop Entry]
 Name=VSCodium
 Type=Application
@@ -37,6 +33,9 @@ Icon=${INSTALL_DIR}/resources/app/resources/linux/code.png
 Comment=VSCodium ${LATEST_VERSION}
 Terminal=false
 NoDisplay=false
-Categories=Development;IDE" | tee "${DESKTOP_FILE}"
+Categories=Development;IDE" > "${DESKTOP_FILE}"
 fi
 
+LATEST_LINK="$(dirname "$INSTALL_DIR")/latest"
+echo "📌 Update latest symbolic link: $LATEST_LINK"
+ln -nfsv "$INSTALL_DIR" "$LATEST_LINK"
