db4f2616b172281ebb8391b4017ff2ea57d2924a
Sébastien MB <seb@essembeh.org>
2023-03-12 23:57:59 +0100

    Fix g810

--- logitech/auto.sh
+++ logitech/auto.sh
@@ -3,6 +3,9 @@
 #  0 * * * * ~/dotfiles/logitech/autoswitch.sh
 set -eu
 
+G810_BIN="$HOME/Projects/g810-led/bin/g810-led"
+test -x "$G810_BIN"
+
 FOLDER=$(dirname "$0")
 
 PROFILE="g810-dark.profile"
@@ -11,5 +14,5 @@ if test $HOUR -ge 8 -a $HOUR -le 20; then
 	PROFILE="g810-light.profile"
 fi
 if test -f "$FOLDER/$PROFILE"; then
-	"$FOLDER/g810-led" -p "$FOLDER/$PROFILE"
+	"$G810_BIN" -p "$FOLDER/$PROFILE"
 fi
