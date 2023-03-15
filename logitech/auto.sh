4a3bff7965a254ed74ef16479e340d171b7671d8
Sébastien MB <seb@essembeh.org>
2023-03-15 01:34:25 +0100

    Update g810-led

--- logitech/auto.sh
+++ logitech/auto.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
-# CRONTAB:
-#  0 * * * * ~/dotfiles/logitech/autoswitch.sh
 set -eu
 
-G810_BIN="$HOME/Projects/g810-led/bin/g810-led"
-test -x "$G810_BIN"
-
-FOLDER=$(dirname "$0")
-
+FOLDER="g810-led"
+REPO="https://github.com/MatMoul/g810-led"
 PROFILE="g810-dark.profile"
 HOUR=$(date +%H)
+
+
+cd "$(dirname "$0")"
+
+if ! test -d "$FOLDER"; then
+    echo "🧲 Clone $REPO --> $FOLDER"
+    git clone "$REPO" "$FOLDER"
+    echo "⏳ Build $FOLDER"
+    nix-shell -p hidapi gnumake --run "cd \"$FOLDER\" && make clean all"
+fi
+
 if test $HOUR -ge 8 -a $HOUR -le 20; then
 	PROFILE="g810-light.profile"
 fi
-if test -f "$FOLDER/$PROFILE"; then
-	"$G810_BIN" -p "$FOLDER/$PROFILE"
-fi
+test -f "$PROFILE"
+test -x "$FOLDER/bin/g810-led"
+"$FOLDER/bin/g810-led" -p "$PROFILE"
+
+
