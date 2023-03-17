5dd1b976274e1fb2b9f1f9ae5a158bedcc05fec5
Sébastien MB <seb@essembeh.org>
2023-03-17 11:31:34 +0100

    Better all in one script

--- logitech/auto.sh
+++ logitech/auto.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 set -eu
 
-FOLDER="g810-led"
-REPO="https://github.com/MatMoul/g810-led"
-PROFILE="g810-dark.profile"
+ROOT=$(dirname "$0")
+TOOL_BIN="$ROOT/g810-led"
+TMPDIR=$(mktemp -d)
+REPO_URL="https://github.com/MatMoul/g810-led"
+PROFILE_NIGHT="$ROOT/g810-dark.profile"
+PROFILE_DAY="$ROOT/g810-light.profile"
+
+if ! test -x "$TOOL_BIN"; then
+    echo "⏳ Build $REPO_URL ..."
+    git clone --quiet "$REPO_URL" "$TMPDIR"
+    nix-shell -p hidapi gnumake --run "cd \"$TMPDIR\" && make clean all"
+    cp -av "$TMPDIR/bin/g810-led" "$TOOL_BIN"
+fi
+
 HOUR=$(date +%H)
-
-
-cd "$(dirname "$0")"
-
-if ! test -d "$FOLDER"; then
-    echo "🧲 Clone $REPO --> $FOLDER"
-    git clone "$REPO" "$FOLDER"
-    echo "⏳ Build $FOLDER"
-    nix-shell -p hidapi gnumake --run "cd \"$FOLDER\" && make clean all"
-fi
-
 if test $HOUR -ge 8 -a $HOUR -le 20; then
-	PROFILE="g810-light.profile"
+    echo "☀ Set day profile"
+    "$TOOL_BIN" -p "$PROFILE_DAY"
+else
+    echo "🌙 Set night profile"
+    "$TOOL_BIN" -p "$PROFILE_NIGHT"
 fi
-test -f "$PROFILE"
-test -x "$FOLDER/bin/g810-led"
-"$FOLDER/bin/g810-led" -p "$PROFILE"
 
 
